f49ea73856c56cdef4a9de31333ed77934c1561e
Rafael-Sabugari <rafaelpolizelo@gmail.com>
2023-11-16 21:49:00 -0300

    wip: create AdministratorTest

---
+++ GOFO Implementation/test/System/AdministratorTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AdministratorTest {
+
+    @Test
+    public void testApprovePlayground() {
+        Administrator admin = new Administrator();
+        Playground playground = new Playground();
+        playground.setName("Playground Teste");
+        playground.setLocation();
+        playground.setOwner("Proprietário Teste");
+        admin.playgroundRequests(playground);
+
+        admin.approvePlayground();
+
+        ArrayList<Playground> approvedList = Administrator.Approved;
+        assertEquals(1, approvedList.size());
+        assertTrue(approvedList.contains(playground));
+    }
+
+    @Test
+    public void testSearchByName() {
+        Administrator admin = new Administrator();
+        Playground playground = new Playground();
+        playground.setName("Playground de Teste");
+        playground.setLocation();
+        playground.setOwner("Dono de Teste");
+        admin.Approved.add(playground);
+
+        admin.searchByName("Playground de Teste");
+        
+    }
+
+    @Test
+    public void testBookByName() {
+        Administrator admin = new Administrator();
+        Playground playground = new Playground();
+        playground.setName("Playground para Reserva");
+        playground.setLocation();
+        playground.setOwner("Proprietário para Reserva");
+        admin.Approved.add(playground);
+
+        int balance = 500;
+        int bookedAmount = admin.bookByName("Playground para Reserva", "Jogador Teste", balance);
+
+        assertEquals(0, bookedAmount); 
+    }
+
+    
+}
