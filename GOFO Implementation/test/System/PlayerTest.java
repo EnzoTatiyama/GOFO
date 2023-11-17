20ccd8a9d023632dcc39a2700abcabee5353bc63
Rafael-Sabugari <rafaelpolizelo@gmail.com>
2023-11-16 21:56:49 -0300

    wip: create PlayerTest

---
+++ GOFO Implementation/test/System/PlayerTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlayerTest {
+
+    private Player player;
+
+    @BeforeEach
+    public void setUp() {
+        player = new Player();
+    }
+
+    @Test
+    public void testSetAndGetBalance() {
+        player.setBalance(500);
+        assertEquals(500, player.getBalance());
+    }
+
+    @Test
+    public void testBookSlots() {
+        player.setBalance(1000);
+        player.bookingSlots("10:00", "Playground A");
+    }
+
+    @Test
+    public void testCancelBooking() {
+        player.setBalance(1000);
+        player.bookingSlots("12:00", "Playground B");
+        player.CancelBooking("Playground B", "12:00");
+    }
+}
