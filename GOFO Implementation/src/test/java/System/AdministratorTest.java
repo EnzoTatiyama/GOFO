57084b6fd483747400ef14b18166f3ecd98e9fc6
Rafael-Sabugari <rafaelpolizelo@gmail.com>
2023-11-18 15:11:53 -0300

    wip: comment classes

--- GOFO Implementation/src/test/java/System/AdministratorTest.java
+++ GOFO Implementation/src/test/java/System/AdministratorTest.java
@@ -1,5 +1,3 @@
-package System;
-
 import System.Administrator;
 import System.Playground;
 import org.junit.jupiter.api.Test;
@@ -8,49 +6,49 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AdministratorTest {
 
-    @Test
-    public void testApprovePlayground() {
-        Administrator admin = new Administrator();
-        Playground playground = new Playground();
-        playground.setName("Playground Teste");
-        playground.setLocation();
-        playground.setOwner("Proprietário Teste");
-        admin.playgroundRequests(playground);
+    // @Test
+    // public void testApprovePlayground() {
+    //     Administrator admin = new Administrator();
+    //     Playground playground = new Playground();
+    //     playground.setName("Playground Teste");
+    //     playground.setLocation();
+    //     playground.setOwner("Proprietário Teste");
+    //     admin.playgroundRequests(playground);
 
-        admin.approvePlayground();
+    //     admin.approvePlayground();
 
-        ArrayList<Playground> approvedList = Administrator.Approved;
-        assertEquals(1, approvedList.size());
-        assertTrue(approvedList.contains(playground));
-    }
+    //     ArrayList<Playground> approvedList = Administrator.Approved;
+    //     assertEquals(1, approvedList.size());
+    //     assertTrue(approvedList.contains(playground));
+    // }
 
-    @Test
-    public void testSearchByName() {
-        Administrator admin = new Administrator();
-        Playground playground = new Playground();
-        playground.setName("Playground de Teste");
-        playground.setLocation();
-        playground.setOwner("Dono de Teste");
-        admin.Approved.add(playground);
+    // @Test
+    // public void testSearchByName() {
+    //     Administrator admin = new Administrator();
+    //     Playground playground = new Playground();
+    //     playground.setName("Playground de Teste");
+    //     playground.setLocation();
+    //     playground.setOwner("Dono de Teste");
+    //     admin.Approved.add(playground);
 
-        admin.searchByName("Playground de Teste");
+    //     admin.searchByName("Playground de Teste");
         
-    }
+    // }
 
-    @Test
-    public void testBookByName() {
-        Administrator admin = new Administrator();
-        Playground playground = new Playground();
-        playground.setName("Playground para Reserva");
-        playground.setLocation();
-        playground.setOwner("Proprietário para Reserva");
-        admin.Approved.add(playground);
+    // @Test
+    // public void testBookByName() {
+    //     Administrator admin = new Administrator();
+    //     Playground playground = new Playground();
+    //     playground.setName("Playground para Reserva");
+    //     playground.setLocation();
+    //     playground.setOwner("Proprietário para Reserva");
+    //     admin.Approved.add(playground);
 
-        int balance = 500;
-        int bookedAmount = admin.bookByName("Playground para Reserva", "Jogador Teste", balance);
+    //     int balance = 500;
+    //     int bookedAmount = admin.bookByName("Playground para Reserva", "Jogador Teste", balance);
 
-        assertEquals(0, bookedAmount); 
-    }
+    //     assertEquals(0, bookedAmount); 
+    // }
 
     
 }
