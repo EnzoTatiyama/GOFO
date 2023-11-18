ba3fc6d519eff64e84cab4d07b0774fe22c9c75b
Rafael-Sabugari <rafaelpolizelo@gmail.com>
2023-11-18 15:05:47 -0300

    wip: changed test classes

--- GOFO Implementation/src/test/java/System/PlayGroundScheduleTest.java
+++ GOFO Implementation/src/test/java/System/PlayGroundScheduleTest.java
@@ -3,40 +3,40 @@ package System;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
+import System.PlayGroundSchedule.*;
 public class PlayGroundScheduleTest {
 
-    // private PlayGroundSchedule schedule;
+    public PlayGroundSchedule schedule;
 
-    // @BeforeEach
-    // public void setUp() {
-    //     schedule = new PlayGroundSchedule();
-    // }
+    @BeforeEach
+    public void setUp() {
+        schedule = new PlayGroundSchedule();
+    }
 
-    // @Test
-    // public void testSetAndGetPrice() {
-    //     schedule.setPrice(); 
-    //     assertEquals(50, schedule.getTimeSlotPerHour());
-    // }
+    @Test
+    public void testSetAndGetPrice() {
+        schedule.setPrice(); 
+        assertEquals(50, schedule.getTimeSlotPerHour());
+    }
 
-    // @Test
-    // public void testSetAndGetSchedule() {
-    //     schedule.setSchedule(); 
-    //     assertEquals(9, schedule.getBegin());
-    //     assertEquals(12, schedule.getEnd());
-    // }
+    @Test
+    public void testSetAndGetSchedule() {
+        schedule.setschedule(); 
+        assertEquals(9, schedule.getBegin());
+        assertEquals(12, schedule.getEnd());
+    }
 
-    // @Test
-    // public void testBookAndDisplaySlots() {
-    //     schedule.setSchedule(); 
-    //     schedule.bookSlot("Player A", "10", "Monday");
-    //     schedule.bookSlot("Player B", "11", "Tuesday");
-    // }
+    @Test
+    public void testBookAndDisplaySlots() {
+        schedule.setschedule(); 
+        schedule.bookSlot("Player A", "10", "Monday");
+        schedule.bookSlot("Player B", "11", "Tuesday");
+    }
 
-    // @Test
-    // public void testUpdateSlot() {
-    //     schedule.setSchedule();
-    //     schedule.bookSlot("Player A", "14", "Wednesday");
-    //     schedule.updateSlot("Wednesday", "14", "Player B");
-    // }
+    @Test
+    public void testUpdateSlot() {
+        schedule.setschedule();
+        schedule.bookSlot("Player A", "14", "Wednesday");
+        schedule.updateSlot("Wednesday", "14", "Player B");
+    }
 }
