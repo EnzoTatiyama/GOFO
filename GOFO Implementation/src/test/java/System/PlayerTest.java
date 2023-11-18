2b3218cebe669546929914331b3a18801a0b1d12
Rafael-Sabugari <rafaelpolizelo@gmail.com>
2023-11-18 16:38:28 -0300

    wip: changed test class

--- GOFO Implementation/src/test/java/System/PlayerTest.java
+++ GOFO Implementation/src/test/java/System/PlayerTest.java
@@ -5,29 +5,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
 
-    private Player player;
+    // private Player player;
 
-    @BeforeEach
-    public void setUp() {
-        player = new Player();
-    }
+    // @BeforeEach
+    // public void setUp() {
+    //     player = new Player();
+    // }
 
-    @Test
-    public void testSetAndGetBalance() {
-        player.setBalance(500);
-        assertEquals(500, player.getBalance());
-    }
+    // @Test
+    // public void testSetAndGetBalance() {
+    //     player.setBalance(500);
+    //     assertEquals(500, player.getBalance());
+    // }
 
-    @Test
-    public void testBookSlots() {
-        player.setBalance(1000);
-        player.bookingSlots("10:00", "Playground A");
-    }
+    // @Test
+    // public void testBookSlots() {
+    //     player.setBalance(1000);
+    //     player.bookingSlots("10:00", "Playground A");
+    // }
 
-    @Test
-    public void testCancelBooking() {
-        player.setBalance(1000);
-        player.bookingSlots("12:00", "Playground B");
-        player.CancelBooking("Playground B", "12:00");
-    }
+    // @Test
+    // public void testCancelBooking() {
+    //     player.setBalance(1000);
+    //     player.bookingSlots("12:00", "Playground B");
+    //     player.CancelBooking("Playground B", "12:00");
+    // }
 }
