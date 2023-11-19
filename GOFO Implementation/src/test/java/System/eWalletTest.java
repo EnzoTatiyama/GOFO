4f38c85717e714df16d8f0c38ca26916d8bd88f5
Enzo Tatiyama <enzo.tatiyama@gmail.com>
2023-11-19 11:18:15 -0300

    fix: eWalletTest

--- GOFO Implementation/src/test/java/System/eWalletTest.java
+++ GOFO Implementation/src/test/java/System/eWalletTest.java
@@ -1,7 +1,5 @@
 package System;
 
-import java.util.Scanner;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,46 +9,34 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class eWalletTest {
     
-//    private eWallet wallet;
-//    private Scanner mockScanner;
-//    
-//    @BeforeEach
-//    public void setUp() {
-//        wallet = new eWallet();
-////        mockScanner = mock(Scanner.class);
-//        wallet.input = mockScanner;
-//    }
-//    
-//    @Test
-//    public void testGetInstance() {
-//        assertEquals(eWallet.class, wallet.getClass());
-//    }
-//
-//    @Test
-//    public void testSetAndBalanceWithStringInput() {
-////        when(mockScanner.nextLine()).thenReturn("100");
-//        wallet.setBalance();
-//        assertEquals(100, wallet.getBalance());
-//    }
-//
-//    @Test
-//    public void testSetAndBalanceWithIntInput() {
-//        wallet.setBalance(100);
-//        assertEquals(100, wallet.getBalance());
-//    }
-//
-//    @Test
-//    public void testDeposit() {
-//        wallet.setBalance(100);
-//        wallet.deposit(10);
-//        assertEquals(110, wallet.getBalance());
-//    }
-//
-//    @Test
-//    public void testWithdraw() {
-//        wallet.setBalance(100);
-//        wallet.withdraw(10);
-//        assertEquals(90, wallet.getBalance());
-//    }
+    
+    @Test
+    public void testGetInstance() {
+        eWallet wallet = new eWallet();
+        assertEquals(eWallet.class, wallet.getClass());
+    }
+
+    @Test
+    public void testSetAndBalanceWithIntInput() {
+        eWallet wallet = new eWallet();
+        wallet.setBalance(100);
+        assertEquals(100, wallet.getBalance());
+    }
+
+    @Test
+    public void testDeposit() {
+        eWallet wallet = new eWallet();
+        wallet.setBalance(100);
+        wallet.deposit(10);
+        assertEquals(110, wallet.getBalance());
+    }
+
+    @Test
+    public void testWithdraw() {
+        eWallet wallet = new eWallet();
+        wallet.setBalance(100);
+        wallet.withdraw(10);
+        assertEquals(90, wallet.getBalance());
+    }
     
 }
