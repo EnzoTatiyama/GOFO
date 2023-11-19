2396aab2268944c5a18afa6974c8001996ba85f9
Enzo Tatiyama <enzo.tatiyama@gmail.com>
2023-11-19 11:40:17 -0300

    fix: PlayGroundScheduleTest

--- GOFO Implementation/src/test/java/System/PlayGroundScheduleTest.java
+++ GOFO Implementation/src/test/java/System/PlayGroundScheduleTest.java
@@ -1,44 +1,22 @@
 package System;
 
+import java.util.Scanner;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayGroundScheduleTest {
 
-//    @Test
-//    public void testGetInstance() {
-//        PlayGroundSchedule playGroundSchedule = new PlayGroundSchedule();
-//        assertEquals(PlayGroundSchedule.class, playGroundSchedule.getClass());
-//    }
-//    
-//    @Test
-//    public void testSetAndGetPrice() {
-//        PlayGroundSchedule playGroundSchedule = new PlayGroundSchedule();
-//        playGroundSchedule.setPrice(); 
-//        assertEquals(50, playGroundSchedule.getTimeSlotPerHour());
-//    }
-//
-//    @Test
-//    public void testSetAndGetSchedule() {
-//        PlayGroundSchedule playGroundSchedule = new PlayGroundSchedule();
-//        playGroundSchedule.setSchedule(); 
-//        assertEquals(9, playGroundSchedule.getBegin());
-//        assertEquals(12, playGroundSchedule.getEnd());
-//    }
-//
-//    @Test
-//    public void testBookAndDisplaySlots() {
-//        PlayGroundSchedule playGroundSchedule = new PlayGroundSchedule();
-//        playGroundSchedule.setSchedule(); 
-//        playGroundSchedule.bookSlot("Player A", "10", "Monday");
-//        playGroundSchedule.bookSlot("Player B", "11", "Tuesday");
-//    }
-//
-//    @Test
-//    public void testUpdateSlot() {
-//        PlayGroundSchedule playGroundSchedule = new PlayGroundSchedule();
-//        playGroundSchedule.setSchedule();
-//        playGroundSchedule.bookSlot("Player A", "14", "Wednesday");
-//        playGroundSchedule.updateSlot("Wednesday", "14", "Player B");
-//    }
+   @Test
+   public void testGetInstance() {
+       PlayGroundSchedule playGroundSchedule = new PlayGroundSchedule();
+       assertEquals(PlayGroundSchedule.class, playGroundSchedule.getClass());
+   }
+   
+   @Test
+   public void testSetAndGetPrice() {
+       PlayGroundSchedule playGroundSchedule = new PlayGroundSchedule();
+       playGroundSchedule.setScanner(new Scanner("20"));
+       playGroundSchedule.setPrice(); 
+       assertEquals(20, playGroundSchedule.getTimeSlotPerHour());
+   }
 }
