f89441596e665cd67c628cfcdc48b542abb5aa4a
Enzo Tatiyama <enzo.tatiyama@gmail.com>
2023-11-19 20:48:44 -0300

    wip: update PlayGroundScheduleTest

--- GOFO Implementation/src/test/java/System/PlayGroundScheduleTest.java
+++ GOFO Implementation/src/test/java/System/PlayGroundScheduleTest.java
@@ -1,14 +1,30 @@
 package System;
 
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 import java.util.Scanner;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayGroundScheduleTest {
 
+   private final InputStream originalSystemIn = System.in;
+
    @Test
    public void testGetInstance() {
        PlayGroundSchedule playGroundSchedule = new PlayGroundSchedule();
+       assertEquals(PlayGroundSchedule.class, playGroundSchedule.getClass());
+   }
+
+   @Test
+   public void testSetPrice() {
+       String input = '20';
+       System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+       PlayGroundSchedule playGroundSchedule = new PlayGroundSchedule();
+       playGroundSchedule.setPrice();
+
+       System.setIn(System.in);
        assertEquals(PlayGroundSchedule.class, playGroundSchedule.getClass());
    }
    
