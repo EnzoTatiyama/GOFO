6adc4284817c6eed775a4b2b6d2db738aab367a6
Enzo Tatiyama <enzo.tatiyama@gmail.com>
2023-11-19 11:09:17 -0300

    fix: PlayGroundScheduleTest

--- GOFO Implementation/src/test/java/System/PlayGroundScheduleTest.java
+++ GOFO Implementation/src/test/java/System/PlayGroundScheduleTest.java
@@ -1,42 +1,44 @@
 package System;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import System.PlayGroundSchedule.*;
+
 public class PlayGroundScheduleTest {
 
-    // public PlayGroundSchedule schedule;
+    @Test
+    public void testGetInstance() {
+        PlayGroundSchedule playGroundSchedule = new PlayGroundSchedule();
+        assertEquals(PlayGroundSchedule.class, playGroundSchedule.getClass());
+    }
+    
+    @Test
+    public void testSetAndGetPrice() {
+        PlayGroundSchedule playGroundSchedule = new PlayGroundSchedule();
+        playGroundSchedule.setPrice(); 
+        assertEquals(50, playGroundSchedule.getTimeSlotPerHour());
+    }
 
-    // @BeforeEach
-    // public void setUp() {
-    //     schedule = new PlayGroundSchedule();
-    // }
+    @Test
+    public void testSetAndGetSchedule() {
+        PlayGroundSchedule playGroundSchedule = new PlayGroundSchedule();
+        playGroundSchedule.setSchedule(); 
+        assertEquals(9, playGroundSchedule.getBegin());
+        assertEquals(12, playGroundSchedule.getEnd());
+    }
 
-    // @Test
-    // public void testSetAndGetPrice() {
-    //     schedule.setPrice(); 
-    //     assertEquals(50, schedule.getTimeSlotPerHour());
-    // }
+    @Test
+    public void testBookAndDisplaySlots() {
+        PlayGroundSchedule playGroundSchedule = new PlayGroundSchedule();
+        playGroundSchedule.setSchedule(); 
+        playGroundSchedule.bookSlot("Player A", "10", "Monday");
+        playGroundSchedule.bookSlot("Player B", "11", "Tuesday");
+    }
 
-    // @Test
-    // public void testSetAndGetSchedule() {
-    //     schedule.setschedule(); 
-    //     assertEquals(9, schedule.getBegin());
-    //     assertEquals(12, schedule.getEnd());
-    // }
-
-    // @Test
-    // public void testBookAndDisplaySlots() {
-    //     schedule.setschedule(); 
-    //     schedule.bookSlot("Player A", "10", "Monday");
-    //     schedule.bookSlot("Player B", "11", "Tuesday");
-    // }
-
-    // @Test
-    // public void testUpdateSlot() {
-    //     schedule.setschedule();
-    //     schedule.bookSlot("Player A", "14", "Wednesday");
-    //     schedule.updateSlot("Wednesday", "14", "Player B");
-    // }
+    @Test
+    public void testUpdateSlot() {
+        PlayGroundSchedule playGroundSchedule = new PlayGroundSchedule();
+        playGroundSchedule.setSchedule();
+        playGroundSchedule.bookSlot("Player A", "14", "Wednesday");
+        playGroundSchedule.updateSlot("Wednesday", "14", "Player B");
+    }
 }
