102dbd81144763162dd0fcebcffe4dbf8e799865
Enzo Tatiyama <enzo.tatiyama@gmail.com>
2023-11-19 11:15:31 -0300

    fix: PlayerTest

--- GOFO Implementation/src/test/java/System/PlayerTest.java
+++ GOFO Implementation/src/test/java/System/PlayerTest.java
@@ -1,31 +1,32 @@
 package System;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
-
-    private Player player;
-
-    @BeforeEach
-    public void setUp() {
-        player = new Player();
+    
+    @Test
+    public void testGetInstance() {
+        Player player = new Player();
+        assertEquals(Player.class, player.getClass());
     }
-
+    
     @Test
     public void testSetAndGetBalance() {
+        Player player = new Player();
         player.setBalance(500);
         assertEquals(500, player.getBalance());
     }
 
     @Test
     public void testBookSlots() {
+        Player player = new Player();
         player.setBalance(1000);
         player.bookingSlots("10:00", "Playground A");
     }
 
     @Test
     public void testCancelBooking() {
+        Player player = new Player();
         player.setBalance(1000);
         player.bookingSlots("12:00", "Playground B");
         player.CancelBooking("Playground B", "12:00");
