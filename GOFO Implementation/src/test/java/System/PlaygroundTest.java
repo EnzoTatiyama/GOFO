e5501066d1ff8ccd5f11ec3434e79cdb79c1ab3b
Rafael-Sabugari <rafaelpolizelo@gmail.com>
2023-11-19 23:40:01 -0300

    wip: 100% coverage PlaygroundTest

--- GOFO Implementation/src/test/java/System/PlaygroundTest.java
+++ GOFO Implementation/src/test/java/System/PlaygroundTest.java
@@ -12,4 +12,95 @@ import java.io.ByteArrayInputStream;
 
 public class PlaygroundTest {
     
+    @Test
+    public void testSetAndGetCancellationPeriod(){
+        Playground playground = new Playground();
+       
+        playground.setCancellationPeriod(2);
+        assertEquals(2, playground.getCancellationPeriod());
+    }
+    
+    @Test
+    public void testBookingTheSlotAndSetGetStatusPlayground(){
+        String simulatedInputOption = "test\nnot available\navailable";
+
+        InputStream in = new ByteArrayInputStream(simulatedInputOption.getBytes());
+        System.setIn(in);
+        
+        Playground playground = new Playground();
+        playground.setStatus();
+        playground.bookingTheSlot("Test","20","20");
+        playground.setStatus();
+        playground.bookingTheSlot("Test","20","20");
+       
+        assertEquals("available", playground.getStatus());
+    }
+                     
+    @Test
+    public void testCancelBooking(){
+        Playground playground = new Playground();
+        playground.cancelBooking("20","20");
+    }
+    
+    @Test
+    public void testFreeSlots(){
+        Playground playground = new Playground();
+        playground.freeSlots();
+    }
+    
+    @Test
+    public void testSetBookingAndGetSlotsBeginEnd(){
+        String simulatedInputOption = "1\n1";
+
+        InputStream in = new ByteArrayInputStream(simulatedInputOption.getBytes());
+        System.setIn(in);
+        
+        Playground playground = new Playground();
+        playground.setBooking();
+        
+        assertEquals(1, playground.getSlotsBegin());
+        assertEquals(1, playground.getSlotsEnd());
+    }
+    
+    @Test
+    public void testSetAndGetOwner(){
+        Playground playground = new Playground();
+        playground.setOwner("Test");
+        
+        assertEquals("Test", playground.getOwner());
+    }
+    
+    @Test
+    public void testSetAndGetName(){
+        Playground playground = new Playground();
+        playground.setName("Test");
+        
+        assertEquals("Test", playground.getName());
+    }
+    
+    @Test
+    public void testSetAndGetLocation(){
+        String simulatedInputOption = "Test";
+
+        InputStream in = new ByteArrayInputStream(simulatedInputOption.getBytes());
+        System.setIn(in);
+        
+        Playground playground = new Playground();
+        playground.setLocation();
+        
+        assertEquals("Test", playground.getLocation());
+    }
+    
+    @Test
+    public void testSetAndGetPrice(){
+        String simulatedInputOption = "1";
+
+        InputStream in = new ByteArrayInputStream(simulatedInputOption.getBytes());
+        System.setIn(in);
+        
+        Playground playground = new Playground();
+        playground.setPrice();
+        
+        assertEquals(1, playground.getPrice());
+    }
 }
