0fb30b1419a9ce220aa5b441b46a5d5683f8a6f7
Rafael-Sabugari <rafaelpolizelo@gmail.com>
2023-11-19 22:55:50 -0300

    wip: 100% coverage PlayerTest

--- GOFO Implementation/src/test/java/System/PlayerTest.java
+++ GOFO Implementation/src/test/java/System/PlayerTest.java
@@ -1,6 +1,9 @@
 package System;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 
 public class PlayerTest {
     
@@ -31,4 +34,52 @@ public class PlayerTest {
         player.bookingSlots("12:00", "Playground B");
         player.CancelBooking("Playground B", "12:00");
     }
+    
+    @Test 
+    public void createTeam(){
+        Player player = new Player();
+        player.createTeam(player);
+        assertEquals(1, player.team.size());
+    }
+    
+    @Test
+    public void testEditPlayerinfo() {
+        String simulatedInputOption = "1\nFirst\n2\nName\n3\n1234\n4\nteste@gmail.com\n5\n82194743\n6\nteste\n7\n1234\n8\n1\nFirst";
+
+        InputStream in = new ByteArrayInputStream(simulatedInputOption.getBytes());
+        System.setIn(in);
+
+        Player player = new Player();
+        player.editPlayerinfo();
+        player.editPlayerinfo();
+        player.editPlayerinfo();
+        player.editPlayerinfo();
+        player.editPlayerinfo();
+        player.editPlayerinfo();
+        player.editPlayerinfo();
+        player.editPlayerinfo();
+        
+        assertEquals("First Name", player.getFullName());
+        assertEquals("1234", player.getPassword());
+        assertEquals("teste@gmail.com", player.getEmail());
+        assertEquals(82194743, player.getPhone());
+        assertEquals("teste", player.location);
+        assertEquals(1234, player.getID());
+    }
+    
+    @Test
+    public void testGetMoney() {
+        Player player = new Player();
+        player.getMoney(5);
+        assertEquals(5, player.getBalance());
+    }
+    
+    @Test
+    public void testSetAndGetInbox() {
+        Player player = new Player();
+        player.viewInbox();
+        player.addInbox("5");
+        player.viewInbox();
+    }
+    
 }
